38e2d6c8c9173b436919d24ac25a0a39a099c7f4
hainamdev <hainamdev@gmail.com>
2022-05-05 20:53:20 +0700

    finish info health

---
+++ app/src/main/java/com/example/sskdt_app_v01/Info_healthActivity.java
@@ -0,0 +1,15 @@
+package com.example.sskdt_app_v01;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Info_healthActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.info_health);
+        
+    }
+}
