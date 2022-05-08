e430b3e6f6d26ffad3c1a651716987cea84c6dad
Nguyễn Nhật Quang <qswaquang@gmail.com>
2022-05-08 16:09:33 +0700

    Umg

--- app/src/main/java/com/example/sskdt_app_v01/InfoUserActivity.java
+++ app/src/main/java/com/example/sskdt_app_v01/InfoUserActivity.java
@@ -1,19 +1,45 @@
 package com.example.sskdt_app_v01;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.IOException;
+import java.util.UUID;
+
 public class InfoUserActivity extends AppCompatActivity {
+    ImageView imgUser;
+    Uri imageUri;
+    FirebaseStorage storage;
+    StorageReference storageRef;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.info_user);
         ConstraintLayout btn_return_info =  findViewById(R.id.btn_return_info);
+
+        storage = FirebaseStorage.getInstance();
+        imgUser = findViewById(R.id.info_user_image);
+
         btn_return_info.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -21,5 +47,74 @@ public class InfoUserActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        imgUser.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                selectedImage();
+
+            }
+        });
+    }
+
+    private void uploadImage() {
+        Log.d("TAG", "uploadImage: aaaaaaaaaaaaaaaaaaaaaaaaaaa");
+        if(imageUri != null)
+        {
+            Log.d("TAG", "uploadImage: begin upload");
+            final ProgressDialog progressDialog = new ProgressDialog(this);
+            progressDialog.setTitle("Uploading...");
+            progressDialog.show();
+
+            storageRef = storage.getReference("images/" + UUID.randomUUID());
+            storageRef.putFile(imageUri)
+                    .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                            progressDialog.dismiss();
+                            Log.d("TAG", "onSuccess: upload success" );
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            progressDialog.dismiss();
+                            Log.d("TAG", "onFailure: upload fail");
+                        }
+                    })
+                    .addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onProgress(UploadTask.TaskSnapshot taskSnapshot) {
+                            double progress = (100.0*taskSnapshot.getBytesTransferred()/taskSnapshot
+                                    .getTotalByteCount());
+                            progressDialog.setMessage("Uploaded "+(int)progress+"%");
+                        }
+                    });
+        }
+    }
+
+    private void selectedImage() {
+        Intent intent = new Intent();
+        intent.setType("image/");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent, "Select Picture"), 100);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == 100 && data != null && data.getData() != null) {
+            imageUri = data.getData();
+            uploadImage();
+            try {
+                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), imageUri);
+                imgUser.setImageBitmap(bitmap);
+            }
+            catch (IOException e)
+            {
+                e.printStackTrace();
+            }
+        }
     }
 }
